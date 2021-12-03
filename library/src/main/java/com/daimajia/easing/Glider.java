98aac7105a49530f098b04eee0d8b99294ddcf44
nonilnull <99825975+nonilnull@users.noreply.github.com>
2021-12-03 22:27:20 +0000

    I18n: Update translation el (100%).

--- library/src/main/java/com/daimajia/easing/Glider.java
+++ library/src/main/java/com/daimajia/easing/Glider.java
@@ -4,4 +4,5 @@
  * Copyright (c) 2014 daimajia
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the S
