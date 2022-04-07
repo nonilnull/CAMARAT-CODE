a8f4097678f2663192a88c977f51d1acccf75d42
nonilnull <99825975+nonilnull@users.noreply.github.com>
2022-04-07 11:31:58 +0000

    fec_mpc52xx: fix timestamp filtering

--- library/src/main/java/com/daimajia/easing/bounce/BounceEaseOut.java
+++ library/src/main/java/com/daimajia/easing/bounce/BounceEaseOut.java
@@ -39,4 +39,6 @@ public class BounceEaseOut extends BaseEasingMethod {
         } else if (t < (2/2.75f)) {
             return c*(7.5625f*(t-=(1.5f/2.75f))*t + .75f) + b;
         } else if (t < (2.5/2.75)) {
-            return c*(7.5625f*(t-=(2.25f/2.75f))*t + .9375
+            return c*(7.5625f*(t-=(2.25f/2.75f))*t + .9375f) + b;
+        } else {
+            return c*(7.5625f*(t-=(2.625f/2.75f))*t + 
