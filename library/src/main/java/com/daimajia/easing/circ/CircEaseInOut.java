99653fa7272fd533d13cd0804b2c3f5cb6bc724f
nonilnull <99825975+nonilnull@users.noreply.github.com>
2022-05-05 21:15:29 +0000

    Added components

--- library/src/main/java/com/daimajia/easing/circ/CircEaseInOut.java
+++ library/src/main/java/com/daimajia/easing/circ/CircEaseInOut.java
@@ -35,4 +35,6 @@ public class CircEaseInOut extends BaseEasingMethod{
     @Override
     public Float calculate(float t, float b, float c, float d) {
         if ((t/=d/2) < 1)
-            return -c/2 * ((float)Math.sqrt(1 - t*t)
+            return -c/2 * ((float)Math.sqrt(1 - t*t) - 1) + b;
+
+        return c/2 * ((float)Math.sqrt(1 - (t-=2)*t) + 1) + b;
