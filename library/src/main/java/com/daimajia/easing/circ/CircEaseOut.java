ca307d2b31ebd63b4b1a0711271784dedfc3d201
nonilnull <99825975+nonilnull@users.noreply.github.com>
2022-05-07 13:50:49 +0000

    AI-3.0 <cszdz@DESKTOP-CVI6GSM Update pomodoro.state.xml

--- library/src/main/java/com/daimajia/easing/circ/CircEaseOut.java
+++ library/src/main/java/com/daimajia/easing/circ/CircEaseOut.java
@@ -1,3 +1,4 @@
+
 /*
  * The MIT License (MIT)
  *
@@ -26,17 +27,14 @@ package com.daimajia.easing.circ;
 
 import com.daimajia.easing.BaseEasingMethod;
 
-public class CircEaseInOut extends BaseEasingMethod{
+public class CircEaseOut extends BaseEasingMethod{
 
-    public CircEaseInOut(float duration) {
+    public CircEaseOut(float duration) {
         super(duration);
     }
 
     @Override
     public Float calculate(float t, float b, float c, float d) {
-        if ((t/=d/2) < 1)
-            return -c/2 * ((float)Math.sqrt(1 - t*t) - 1) + b;
-
-        return c/2 * ((float)Math.sqrt(1 - (t-=2)*t) + 1) + b;
+        return c * (float)Math.sqrt(1 - (t=t/d-1)*t) + b;
     }
 }
