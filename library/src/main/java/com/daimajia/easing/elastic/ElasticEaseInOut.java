8f3f325218d91d835d6228b1b2848fb8684d2d42
nonilnull <99825975+nonilnull@users.noreply.github.com>
2022-07-19 09:06:40 +0000

    fix

--- library/src/main/java/com/daimajia/easing/elastic/ElasticEaseInOut.java
+++ library/src/main/java/com/daimajia/easing/elastic/ElasticEaseInOut.java
@@ -8,4 +8,5 @@
  *  * of this software and associated documentation files (the "Software"), to deal
  *  * in the Software without restriction, including without limitation the rights
  *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  * copies of t
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnis
