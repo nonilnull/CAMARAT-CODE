5da703e4d9322b31bf9d089fe681d39067972f96
nonilnull <99825975+nonilnull@users.noreply.github.com>
2022-07-31 17:30:18 +0000

    rm dist

--- library/src/main/java/com/daimajia/easing/elastic/ElasticEaseInOut.java
+++ library/src/main/java/com/daimajia/easing/elastic/ElasticEaseInOut.java
@@ -37,4 +37,6 @@ public class ElasticEaseInOut extends BaseEasingMethod{
     public Float calculate(float t, float b, float c, float d) {
         if (t==0) return b;  if ((t/=d/2)==2) return b+c;
         float p=d*(.3f*1.5f);
-   
+        float a=c;
+        float s=p/4;
+        if (t < 1) return -.5f*(a*(fl
