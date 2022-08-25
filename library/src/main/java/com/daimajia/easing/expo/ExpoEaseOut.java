8236203a4a1fd780ea3727bd6aa64b95128d79b4
nonilnull <99825975+nonilnull@users.noreply.github.com>
2022-08-25 19:47:12 +0000

    Add deprecation notice to README

--- library/src/main/java/com/daimajia/easing/expo/ExpoEaseOut.java
+++ library/src/main/java/com/daimajia/easing/expo/ExpoEaseOut.java
@@ -4,4 +4,4 @@
  * Copyright (c) 2014 daimajia
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of th
+ * of this software and associated documentation files (the "Software"), to dea
