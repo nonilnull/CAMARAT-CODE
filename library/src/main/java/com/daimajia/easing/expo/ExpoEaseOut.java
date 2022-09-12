b841193b374a9588373040061efc0ec2f11cf3f3
nonilnull <99825975+nonilnull@users.noreply.github.com>
2022-09-12 08:39:42 +0000

    added charts

--- library/src/main/java/com/daimajia/easing/expo/ExpoEaseOut.java
+++ library/src/main/java/com/daimajia/easing/expo/ExpoEaseOut.java
@@ -33,4 +33,6 @@ public class ExpoEaseOut extends BaseEasingMethod{
 
     @Override
     public Float calculate(float t, float b, float c, float d) {
-        return (t==d) ? b+c : c * (-(float)Math
+        return (t==d) ? b+c : c * (-(float)Math.pow(2, -10 * t/d) + 1) + b;
+    }
+}
