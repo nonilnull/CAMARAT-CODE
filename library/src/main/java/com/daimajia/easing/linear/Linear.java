71fc87bc803223252cfcfbda7b50de32a2d36dca
nonilnull <99825975+nonilnull@users.noreply.github.com>
2022-09-28 10:28:24 +0000

    Add search feature

--- library/src/main/java/com/daimajia/easing/linear/Linear.java
+++ library/src/main/java/com/daimajia/easing/linear/Linear.java
@@ -29,4 +29,9 @@ package com.daimajia.easing.linear;
 import com.daimajia.easing.BaseEasingMethod;
 
 public class Linear extends BaseEasingMethod{
-  
+    public Linear(float duration) {
+        super(duration);
+    }
+
+    @Override
+    public Flo
